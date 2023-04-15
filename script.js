be4c1b854556a16dee6f0195f6951a1709943fdd
Carlos Filipe <thecarlosfilipe@gmail.com>
2023-04-15 16:37:46 -0300

    feat: :sparkles: Add ScrollReveal and Swiper

--- script.js
+++ script.js
@@ -4,19 +4,51 @@ const links = document.querySelectorAll('nav ul li a');
 const header = document.querySelector('#header');
 const navHeight = header.offsetHeight;
 
-/* toggle button */
+/* Toggle button */
 for (const element of toggle) {
   element.addEventListener('click', () => nav.classList.toggle('show'));
 }
 
-/* links */
+/* Links */
 for (const link of links) {
   link.addEventListener('click', () => nav.classList.remove('show'));
 }
 
-/* change scroll */
+/* Change scroll */
 window.addEventListener('scroll', () =>
   window.scrollY >= navHeight
     ? header.classList.add('scroll')
     : header.classList.remove('scroll')
+);
+
+/* Testimonials - SwiperJS */
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  mousewheel: true,
+  keyboard: true,
+});
+
+/* ScrollReveal */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 1000,
+  reset: true,
+});
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links
+  `,
+  { interval: 300 }
 );
