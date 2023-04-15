a2608dce939605c3b88f7c11c49d5bfbdad856c8
Carlos Filipe <thecarlosfilipe@gmail.com>
2023-04-14 23:41:32 -0300

    feat: :sparkles: Add toggle menu

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Flamengoooooo!');
+const nav = document.querySelector('.container');
+const toggle = document.querySelectorAll('nav .toggle');
+const links = document.querySelectorAll('nav ul li a');
+
+/* toggle button */
+for (const element of toggle) {
+  element.addEventListener('click', () => nav.classList.toggle('show'));
+}
+
+/* links */
+for (const link of links) {
+  link.addEventListener('click', () => nav.classList.remove('show'));
+}
