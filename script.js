c518f0120e5d90741c1d68d8ed7410447deb063d
Carlos Filipe <thecarlosfilipe@gmail.com>
2023-04-15 00:25:35 -0300

    feat: :construction: Work in progress

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const nav = document.querySelector('.container');
 const toggle = document.querySelectorAll('nav .toggle');
 const links = document.querySelectorAll('nav ul li a');
+const header = document.querySelector('#header');
+const navHeight = header.offsetHeight;
 
 /* toggle button */
 for (const element of toggle) {
@@ -11,3 +13,10 @@ for (const element of toggle) {
 for (const link of links) {
   link.addEventListener('click', () => nav.classList.remove('show'));
 }
+
+/* change scroll */
+window.addEventListener('scroll', () =>
+  window.scrollY >= navHeight
+    ? header.classList.add('scroll')
+    : header.classList.remove('scroll')
+);
