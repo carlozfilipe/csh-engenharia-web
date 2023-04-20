0bc7cbb5f7bc6fef22343bd9e6530159ce3f5138
Carlos Filipe <thecarlosfilipe@gmail.com>
2023-04-19 21:13:52 -0300

    fix: :bug: ScrollReveal fixed

--- script.js
+++ script.js
@@ -40,14 +40,14 @@ const scrollReveal = ScrollReveal({
   origin: 'top',
   distance: '30px',
   duration: 700,
-  reset: true,
+  reset: true
 });
 
 scrollReveal.reveal(
   `#home .image, #home .text,
   #about .image, #about .text,
   #services header, #services .card,
-  #testimonials header, #testimonials .testimonials,
+  #testimonials header, #testimonials .testimonials
   #contact .text, #contact .links
   `,
   { interval: 100 }
